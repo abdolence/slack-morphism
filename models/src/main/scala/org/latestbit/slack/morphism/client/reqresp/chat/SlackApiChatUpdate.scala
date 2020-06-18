68b9dd9db42949055abc958c3ab6d17593fb9bfa
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2020-06-18 18:05:08 +0300

    chat.update as_user fix

--- models/src/main/scala/org/latestbit/slack/morphism/client/reqresp/chat/SlackApiChatUpdate.scala
+++ models/src/main/scala/org/latestbit/slack/morphism/client/reqresp/chat/SlackApiChatUpdate.scala
@@ -27,7 +27,7 @@ case class SlackApiChatUpdateRequest(
     channel: String,
     text: String,
     ts: String,
-    as_user: String,
+    as_user: Option[String] = None,
     blocks: Option[List[SlackBlock]] = None,
     link_names: Option[Boolean] = None,
     parse: Option[String] = None
